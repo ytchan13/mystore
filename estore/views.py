69beb5f8379930c4a3fcea5b0aebe3b2fb741cc4
ting <ytchan66@gmail.com>
2017-08-06 13:04:47 +0800

    update hw1

--- estore/views.py
+++ estore/views.py
@@ -39,7 +39,7 @@ class ProductDetail(generic.DetailView):
 class ProductCreate(PermissionRequiredMixin, generic.CreateView):
     permission_required = 'estore.add_product'
     model = Product
-    fields = ('title', 'description', 'quantity', 'price','image')
+    fields = ('title', 'description', 'quantity', 'price', 'image')
 
     def get_success_url(self):
         messages.success(self.request, '產品已新增')
@@ -59,7 +59,7 @@ class ProductCreate(PermissionRequiredMixin, generic.CreateView):
 class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'estore.change_product'
     model = Product
-    fields = ('title', 'description', 'quantity', 'price','image')
+    fields = ('title', 'description', 'quantity', 'price', 'image')
 
     def get_success_url(self):
         messages.success(self.request, '產品已變更')
