6c37e19f3a31ed06bcfe567b68d847a087c78783
ting <ytchan66@gmail.com>
2017-08-06 13:32:03 +0800

    modify settings.py

--- mystore/settings.py
+++ mystore/settings.py
@@ -129,7 +129,7 @@ LOGIN_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
